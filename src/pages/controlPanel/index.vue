fa8d1c70f6074bb0380024f63928bf8704d9daa4
LeonCF <1037299165@qq.com>
2018-05-15 14:55:45 +0800

    官方注释删除（破电脑的git又坏了，调试）

--- src/pages/controlPanel/index.vue
+++ src/pages/controlPanel/index.vue
@@ -1,24 +1,5 @@
 <template>
   <div class="container" @click="clickHandle('test click', $event)">
-
-    <!-- <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">控制面板示例页面</a> -->
     <div class="terminal">
         <p class="terminal_p">终端</p>
         <div class="terminal-area">
